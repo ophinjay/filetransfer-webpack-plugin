ff76744e97d5e35d109cfac32324f802824df6e9
Jophin Joseph <jophin.joseph@myntra.com>
2015-10-13 09:49:09 +0530

    added fs-extra

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var path = require('path');
-var fs = require('fs');
+var fs = require('fs-extra');
 
 function FileTransferPlugin(pattern, basePath) {
     this.pattern = pattern || [];
@@ -9,22 +9,20 @@ function FileTransferPlugin(pattern, basePath) {
 FileTransferPlugin.prototype.apply = function(compiler) {
     var _this = this;
     var basePath = this.basePath || compiler.options.context || null;
-
+    console.log(basePath);
     compiler.plugin('emit', function(compilation, cb) {
         if (!basePath) {
             compilation.errors.push(new Error('FileTransferPlugin: no basePath provided'));
             cb();
         }
+        var src = path.join(basePath, _this.pattern.src);
 
-        compilation.assets[_this.pattern.dest] = {
-            size: function() {
-                return fs.statSync(path.resolve(basePath, _this.pattern.src)).size;
-            },
-            source: function() {
-                return fs.readFileSync(_this.pattern.src);
-            },
-        };
-
+        fs.copy(src, _this.pattern.dest, function(err) {
+            if (err) {
+                return console.error(err)
+            }
+            console.log("success!")
+        });
     });
 };
 
